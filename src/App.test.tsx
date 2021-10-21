612ad19b36bc76745b081528dd4b4e5780d89544
Raúl Guadarrama <r.guadarrama.recio@accenture.com>
2021-10-21 09:49:32 +0200

    clear code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  waitForElement,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 // NOTE: SAMPLE
@@ -19,53 +13,3 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-// describe('marvel/services', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
-
-//   // this test passes perfectly
-//   it('Calls action when 1st form btn is clicked', async () => {
-//     const { getByText, getByLabelText } = render(<App />);
-
-//     const firstInput = getByLabelText('1st input label');
-//     const firstButton = getByText('1st button text');
-
-//     fireEvent.change(firstInput, { target: { value: 'blablabla' } });
-
-//     await waitFor(() => {
-//       fireEvent.click(firstButton);
-//     });
-
-//     waitFor(() => {
-//       expect(testingCallbackFuncStep1).toBe(1);
-//     });
-//   });
-//   // here I got the problem
-//   it('Calls action when 2nd form btn is clicked', async () => {
-//     const { getByText, getByLabelText } = render(...My component...);
-
-//     const _firstInput = getByLabelText('1st input label');
-//     const _firstButton = getByText('1st button text');
-
-//     fireEvent.change(_firstInput, { target: { value: 'blablabla' } });
-
-//     await waitFor(() => {
-//       fireEvent.click(_firstButton);
-//     });
-
-//     await waitForElement(() => [getByLabelText('2nd input label'), getByText('2nd button text')]);
-
-//     // from here to the end of the suite, nothing is triggered
-//     fireEvent.change(getByLabelText('2nd input label'), { target: { value: 'blablabla 2' } });
-
-//     await waitFor(() => {
-//       fireEvent.click(getByText('2nd button text'));
-//     });
-
-//     waitFor(() => {
-//       expect(testingCallbackFuncStep2).toBe(1);
-//     });
-//   });
-// });
